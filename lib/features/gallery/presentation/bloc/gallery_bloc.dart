433197a253392d36d23aba4a17988593acdd0f29
Ne'matjon <118611615+MadazimovNematjon@users.noreply.github.com>
2025-01-17 19:49:12 +0500

    comment

--- lib/features/gallery/presentation/bloc/gallery_bloc.dart
+++ lib/features/gallery/presentation/bloc/gallery_bloc.dart
@@ -1,37 +1,42 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gallery_app/my_library.dart';
 
+// GalleryBloc - galereya ekranidagi media aktivlarini boshqaruvchi BLoC
 class GalleryBloc extends Bloc<GalleryEvent, GalleryState> {
-  final MediaAssetsUseCase mediaAssetsUseCase;
+  final MediaAssetsUseCase mediaAssetsUseCase; // Media aktivlarini olish uchun xizmat
 
-
+  // Konstruktor orqali MediaAssetsUseCase'ni yuborib, bosh holat sifatida GalleryInitial o'rnatiladi
   GalleryBloc({
     required this.mediaAssetsUseCase,
-
   }) : super(GalleryInitial()) {
-    // Bu qator Bloc qanday eventlarni kutishini va bu eventlar kelganda qanday logikani bajarishini belgilaydi.
-    on<GetMediaAssetsEvent>(_getAssets);
+    // Bloc qanday eventlarni kutishini va ularning bajarilishini belgilash
+    on<GetMediaAssetsEvent>(_getAssets); // GetMediaAssetsEvent kelganda _getAssets metodini chaqirish
   }
 
-
+  // Media aktivlarini olish uchun ishlatiladigan metod
   Future<void> _getAssets(
       GetMediaAssetsEvent event, Emitter<GalleryState> emit) async {
     try {
+      // Yuklanayotgan holatni yuborish
       emit(GalleryLoading());
+
+      // Media aktivlarini olish uchun use case'ni chaqirish
       final result = await mediaAssetsUseCase.call(NoParams());
-      result.fold((failure) {
-        emit(GalleryError(failure.message));
-      }, (response) {
-        emit(GallerySuccess(
-            response)); // Rasm fayllari muvaffaqiyatli yuklandi
-      });
+
+      // Natijani tekshirish
+      result.fold(
+            (failure) {
+          // Agar xato bo'lsa, GalleryError holatini yuborish
+          emit(GalleryError(failure.message));
+        },
+            (response) {
+          // Agar muvaffaqiyatli bo'lsa, GallerySuccess holatini yuborish
+          emit(GallerySuccess(response)); // Rasm fayllari muvaffaqiyatli yuklandi
+        },
+      );
     } catch (e) {
+      // Agar hatolik yuzaga kelsa, xatoni qaytarish
       emit(GalleryError(e.toString()));
     }
   }
-
-
-
-
-
 }
--- lib/features/gallery/presentation/ui/gallery_screen.dart
+++ lib/features/gallery/presentation/ui/gallery_screen.dart
@@ -6,6 +6,7 @@ import 'package:gallery_app/features/gallery/presentation/bloc/gallery_bloc.dart
 import 'package:gallery_app/features/gallery/presentation/bloc/gallery_event.dart';
 import 'package:gallery_app/features/gallery/presentation/bloc/gallery_state.dart';
 
+// GalleryScreen widget - bu galereya ekranining UI qismini saqlovchi StatefulWidget
 class GalleryScreen extends StatefulWidget {
   const GalleryScreen({super.key});
 
@@ -16,35 +17,52 @@ class GalleryScreen extends StatefulWidget {
 class _GalleryScreenState extends State<GalleryScreen> {
   @override
   Widget build(BuildContext context) {
+    // BlocBuilder orqali GalleryBloc'ning holatini tinglab, UI'ni yangilash
     return BlocBuilder<GalleryBloc, GalleryState>(
+      // Ekran yuklanganda media assets (rasmlar) olish uchun voqeani ishga tushiramiz
       bloc: context.read<GalleryBloc>()..add(GetMediaAssetsEvent()),
+
+      // Holat o'zgarganda UI'ni qayta quradigan builder
       builder: (context, state) {
+
+        // Agar holat GalleryInitial bo'lsa, bo'sh konteyner qaytariladi
         if (state is GalleryInitial) {
           return Container();
         }
+
+        // Agar holat GalleryLoading bo'lsa, yuklanayotgan spinnerni ko'rsatish
         if (state is GalleryLoading) {
           return Center(
             child: CircularProgressIndicator(),
           );
         }
+
+        // Agar holat GallerySuccess bo'lsa, rasm va videolarni guruhlangan holda ko'rsatish
         if (state is GallerySuccess) {
+          // AppServices yordamida rasmlarni sana bo'yicha guruhlash
           var data = AppServices.groupImagesByDate(state.data);
+
+          // MediaItems widget'ini qaytarish, bunda guruhlangan rasmlar ko'rsatiladi
           return MediaItems(
-            title:"Photo",
-            data: data,
-            ungroupedImages: state.data,
+            title: "Photo", // Title sifatida "Photo" qo'yiladi
+            data: data, // Guruhlangan ma'lumotlar uzatiladi
+            ungroupedImages: state.data, // Guruhlanmagan rasmlar ham uzatiladi
           );
         }
+
+        // Agar holat GalleryError bo'lsa, xato xabarini ko'rsatish
         if (state is GalleryError) {
-          Center(
+          return Center(
             child: Text(
-              "Error",
-              style: TextStyle(fontSize: 50, color: Colors.red),
+              "Error", // Xato xabari
+              style: TextStyle(fontSize: 50, color: Colors.red), // Xato xabarini qizil rangda va katta harfda ko'rsatish
             ),
           );
         }
+
+        // Agar boshqa holatlar bo'lsa, umumiy xato xabarini ko'rsatish
         return Center(
-          child: Text("Noqaniq hatolik"),
+          child: Text("Noqaniq hatolik"), // Umumiy xato xabari
         );
       },
     );
